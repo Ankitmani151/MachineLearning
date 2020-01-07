2fa6ed05282251cb3bf642ae6a45d50898ce535f
Ankit Mani <55287567+Ankitmani151@users.noreply.github.com>
2020-01-08 00:07:30 +0530

    Update Data_Preprocessing.r

--- Data_Preprocessing/Data_Preprocessing.r
+++ Data_Preprocessing/Data_Preprocessing.r
@@ -1,7 +1,10 @@
 # Data Preprocessing
 
 # Importing the dataset
+setwd("C:\\Users\\A691602\\Desktop\\Python\\my example\\Machine Learning\\Machine Learning A-Z New\\Part 1 - Data Preprocessing\\Section 2 -------------------- Part 1 - Data Preprocessing --------------------")
 dataset = read.csv('Data.csv')
+
+
 
 # Taking care of missing data
 dataset$Age = ifelse(is.na(dataset$Age),
@@ -18,15 +21,15 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                            levels = c('No', 'Yes'),
                            labels = c(0, 1)) 
-						   
+
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[,2:3] = scale(training_set)
-test_set[,2:3] = scale(test_set)
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
